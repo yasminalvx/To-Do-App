0c2ab9245a71b9e7ba0c3ab1d096becb53eac0db
Yasmin Alves <yasilvalves@gmail.com>
2022-02-03 17:15:02 -0300

    to-do app

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from './todo.model';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  public todos: Todo[] = [];
+  public form!: FormGroup;
+  public mode: String = 'list';
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+    this.load();
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+    this.changeMode('list');
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  changeMode(mode: String) {
+    this.mode = mode;
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
